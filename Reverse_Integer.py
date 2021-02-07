5bc617b5533710d5dc8472597f52f6e2337dbe7c
josujean <44385491+josujean@users.noreply.github.com>
2021-02-06 21:11:18 -0600

    Create Reverse_Integer.py

---
+++ Reverse_Integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            x_sign = -1
+            x_new = str(x)[1:]
+        else:
+            x_sign = 1
+            x_new = str(x)
+
+        x_new = x_sign * int("".join(x_new[::-1]))
+        if abs(x_new) > 2**31:
+            return 0
+        else:
+            return x_new
